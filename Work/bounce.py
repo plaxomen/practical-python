2128740363c6de01ae265ddfe8af8923613417df
plaxomen <samuel.piedra@protonmail.com>
2024-03-26 09:01:33 -0700

    Complete Exercise 1.5: The Bouncing Ball

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+
+# we have a starting height of 100 meters, referred to as drop_height
+# each time the ball bounces from a drop_height the bounce height is equal to (3/5) of drop_height
+# print a table showing the height of the first 10 bounces.
+
+bounce_ratio = 3 / 5
+bounce_height = 100
+for b in range(1, 11):
+    bounce_height = bounce_height * bounce_ratio
+    print(f"{b} {round(bounce_height, 4)}")
