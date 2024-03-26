87817de2a8d2478b49987ea1566f276002aab02b
plaxomen <samuel.piedra@protonmail.com>
2024-03-26 11:24:20 -0700

    Completed Exercise 1.17: f-strings

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -11,8 +11,8 @@ extra_payment_start_month = 61
 extra_payment_end_month = 108
 extra_payment = 1000
 
-print("Months\tTotal Paid\tRemaining Principal")
-print("-" * 60)
+print(f"Months | Total Paid ($) | Remaining Principal ($)")
+print("-" * 55)
 while principal > 0:
     months += 1
     if principal < minimum_payment:
@@ -23,8 +23,8 @@ while principal > 0:
         payment = minimum_payment
     principal = principal * (1 + rate / 12) - payment
     total_paid = total_paid + payment
-    print(f"{months}\t| ${round(total_paid, 2)}\t| ${round(principal, 2)}")
+    print(f"{months:<6} | ${total_paid:<13,.2f} | ${principal:,.2f}")
 
 
-print(f"Total Paid: ${round(total_paid, 2)}")
+print(f"Total Paid: ${total_paid:,.2f}")
 print(f"Months: {months}")
