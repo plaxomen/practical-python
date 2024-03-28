989bd6ee8d7e38d762dfe9ca673c937f007852cd
plaxomen <samuel.piedra@protonmail.com>
2024-03-27 17:45:09 -0700

    Completed Exercise 1.30: Turning a script into a function

--- Work/pcost.py
+++ Work/pcost.py
@@ -4,14 +4,19 @@
 #
 # Write a program to calculate the total cost of the holdings in portfolio.csv
 
-total = 0
-with open("Data/portfolio.csv", "rt") as f:
-    header = next(f)
-    for line in f:
-        row = line.split(",")
-        nshares = int(row[1])
-        price = float(row[2])
-        cost = nshares * price
-        total += cost
 
-print(f"Total cost: ${total:,.2f}")
+def portfolio_cost(filename):
+    "A function to calculate the total cost of a portfolio in a file."
+    total = 0
+    with open(filename, "rt") as f:
+        header = next(f)
+        for line in f:
+            row = line.split(",")
+            nshares = int(row[1])
+            price = float(row[2])
+            total += nshares * price
+    return total
+
+
+cost = portfolio_cost("Data/portfolio.csv")
+print(f"Total cost: ${cost:,.2f}")
