40550d92b7a04415e2f59a652e763cf5ff57e136
plaxomen <samuel.piedra@protonmail.com>
2024-03-27 17:11:16 -0700

    Completed Exercise 1.27: Reading a data file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+#
+# Write a program to calculate the total cost of the holdings in portfolio.csv
+
+total = 0
+with open("Data/portfolio.csv", "rt") as f:
+    header = next(f)
+    for line in f:
+        row = line.split(",")
+        nshares = int(row[1])
+        price = float(row[2])
+        cost = nshares * price
+        total += cost
+
+print(f"Total cost: ${total:,.2f}")
