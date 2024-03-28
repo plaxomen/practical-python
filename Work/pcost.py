7e8f0622ef5399913de33c7d6f660bb74787f6d6
plaxomen <samuel.piedra@protonmail.com>
2024-03-27 18:03:06 -0700

    Completed Exercise 1.32: Using a library function

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,21 +3,22 @@
 # Exercise 1.27
 #
 # Write a program to calculate the total cost of the holdings in portfolio.csv
+import csv
 
 
 def portfolio_cost(filename):
     "A function to calculate the total cost of a portfolio in a file."
     total = 0
     with open(filename, "rt") as f:
-        header = next(f)
-        for line_no, line in enumerate(f, 2):
+        rows = csv.reader(f)
+        header = next(rows)
+        for row_no, row in enumerate(rows, 2):
             try:
-                row = line.split(",")
                 nshares = int(row[1])
                 price = float(row[2])
                 total += nshares * price
             except Exception as e:
-                print(f"Error processing row[{line_no}]. {e}")
+                print(f"Error processing row[{row_no}]. {e}")
     return total
 
 
