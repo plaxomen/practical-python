3a4553aeeaa71ae8b666fec9e76f3008751392d3
plaxomen <samuel.piedra@protonmail.com>
2024-03-31 12:53:03 -0700

    Completed Exercise 2.4: A list of tuples

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,17 @@
 # report.py
 #
 # Exercise 2.4
+
+import sys
+import csv
+
+
+def read_portfolio(filename):
+    """A function that returns a list of tuples of the holdings in a portfolio"""
+    portfolio = []
+    with open(filename, "rt") as f:
+        data = csv.reader(f)
+        header = next(data)
+        for row in data:
+            portfolio.append((row[0], int(row[1]), float(row[2])))
+    return portfolio
