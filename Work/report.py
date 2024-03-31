95408282bc84ec8b8def2ffda392673f27923262
plaxomen <samuel.piedra@protonmail.com>
2024-03-31 13:57:37 -0700

    Completed Exercise 2.7: Finding out if you can retire

--- Work/report.py
+++ Work/report.py
@@ -2,7 +2,6 @@
 #
 # Exercise 2.4
 
-import sys
 import csv
 
 
@@ -28,3 +27,65 @@ def read_portfolio_dict(filename):
                 {"name": row[0], "shares": int(row[1]), "price": float(row[2])}
             )
     return portfolio
+
+
+def read_prices(filename):
+    """Returns a dictionary of stock prices (key=stock name, value=stock price)
+    read from a file."""
+    prices = {}
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        for row in rows:
+            if row:
+                prices[row[0]] = float(row[1])
+    return prices
+
+
+def compute_portfolio_cost(portfolio):
+    """Returns the cost of the entire portfolio."""
+    total = 0.0
+    for stock in portfolio:
+        total += stock["shares"] * stock["price"]
+    return total
+
+
+def compute_portfolio_value(portfolio, prices):
+    total = 0.0
+    for stock in portfolio:
+        name = stock["name"]
+        if name in prices:
+            total += stock["shares"] * prices[name]
+    return total
+
+
+def compute_portfolio_pnl(portfolio, prices):
+    cost = compute_portfolio_cost(portfolio)
+    current_value = compute_portfolio_value(portfolio, prices)
+    return current_value - cost
+
+
+def compute_holding_pnl(portfolio, prices):
+    """Returns a dictionary of the pnl for each holding in the portfolio."""
+    pnl = {}
+    for stock in portfolio:
+        name = stock["name"]
+        if name in prices:
+            cost = stock["shares"] * stock["price"]
+            curr_value = stock["shares"] * prices[name]
+            pl = round(curr_value - cost, 2)
+            if name in pnl:
+                pnl[name] += pl
+            else:
+                pnl[name] = pl
+        else:
+            pnl[name] = None
+    return pnl
+
+
+portfolio = read_portfolio_dict("Data/portfolio.csv")
+prices = read_prices("Data/prices.csv")
+
+print(f"Portfolio Cost: ${compute_portfolio_cost(portfolio):,.2f}")
+print(f"Portfolio Value: ${compute_portfolio_value(portfolio, prices):,.2f}")
+print(f"Portfolio PNL: ${compute_portfolio_pnl(portfolio, prices):,.2f}")
