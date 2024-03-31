b3f12fa31467825bed98d1f36f4d351f923c364a
plaxomen <samuel.piedra@protonmail.com>
2024-03-31 13:06:15 -0700

    Completed Exercise 2.5: List of Dictionaries

--- Work/report.py
+++ Work/report.py
@@ -6,7 +6,7 @@ import sys
 import csv
 
 
-def read_portfolio(filename):
+def read_portfolio_tuple(filename):
     """A function that returns a list of tuples of the holdings in a portfolio"""
     portfolio = []
     with open(filename, "rt") as f:
@@ -14,4 +14,17 @@ def read_portfolio(filename):
         header = next(data)
         for row in data:
             portfolio.append((row[0], int(row[1]), float(row[2])))
+    return portfolio
+
+
+def read_portfolio_dict(filename):
+    """A function that returns a list of dictionaries of the holdings in a portfolio"""
+    portfolio = []
+    with open(filename, "rt") as f:
+        data = csv.reader(f)
+        header = next(data)
+        for row in data:
+            portfolio.append(
+                {"name": row[0], "shares": int(row[1]), "price": float(row[2])}
+            )
     return portfolio
