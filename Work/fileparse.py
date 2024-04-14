f4827a01d187ed71e7f2940d1d3cb43d870a9252
plaxomen <samuel.piedra@protonmail.com>
2024-04-14 12:56:07 -0700

    Completed Exercise 3.4: Building a Column Selector

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,18 +4,30 @@
 import csv
 
 
-def parse_csv(filename: str) -> dict:
+def parse_csv(filename: str, select=None) -> dict:
     """A function that takes a csv filename as input and returns a
     list of dictionaries representing each row of data."""
     with open(filename, "rt") as f:
         rows = csv.reader(f)
 
-        # Read the file headers.
+        # Read file headers.
         header = next(rows)
+
+        # If select columns provided, find the indices of the columns names from existing headers.
+        if select:
+            indices = [header.index(colname) for colname in select]
+            header = select
+        else:
+            indices = []
+
         records = []
         for row in rows:
             if not row:  # Skip empty rows.
                 continue
+            if indices:
+                row = [row[index] for index in indices]
+
+            # Build Dictionary
             record = dict(zip(header, row))
             records.append(record)
     return records
