830660d8e475c1387f3bc26b67637b81c8dc0813
plaxomen <samuel.piedra@protonmail.com>
2024-04-17 07:53:47 -0700

    Completed Exercise 3.17: From filename to file-like objects.

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -5,7 +5,7 @@ import csv
 
 
 def parse_csv(
-    filename: str,
+    iterable,
     select=None,
     types=None,
     has_headers=True,
@@ -18,44 +18,41 @@ def parse_csv(
         raise RuntimeError(
             f"'select' column headers provided ({select}) for file containing no headers."
         )
+    rows = csv.reader(iterable, delimiter=delimiter)
 
-    with open(filename, "rt") as f:
-        rows = csv.reader(f, delimiter=delimiter)
+    # If file has headers then handle for headers and possible select columns.
+    if has_headers:
+        # Read file headers.
+        header = next(rows)
 
-        # If file has headers then handle for headers and possible select columns.
-        if has_headers:
-            # Read file headers.
-            header = next(rows)
+        # If select columns provided, find the indices of the columns names from existing headers.
+        if select:
+            indices = [header.index(colname) for colname in select]
+            header = select
+        else:
+            indices = []
 
-            # If select columns provided, find the indices of the columns names from existing headers.
-            if select:
-                indices = [header.index(colname) for colname in select]
-                header = select
+    records = []
+    for rowno, row in enumerate(rows, start=1):
+        try:
+            if not row:  # Skip empty rows.
+                continue
+
+            # Filter the row if specific columns were selected
+            if has_headers and indices:
+                row = [row[index] for index in indices]
+
+            # Apply type conversions of specific types were provided
+            if types:
+                row = [func(value) for func, value in zip(types, row)]
+
+            # Build dictionay if headers supplied or tuples if missing headers
+            if has_headers:
+                record = dict(zip(header, row))
             else:
-                indices = []
-
-        records = []
-        for rowno, row in enumerate(rows, start=1):
-            try:
-                if not row:  # Skip empty rows.
-                    continue
-
-                # Filter the row if specific columns were selected
-                if has_headers and indices:
-                    row = [row[index] for index in indices]
-
-                # Apply type conversions of specific types were provided
-                if types:
-                    row = [func(value) for func, value in zip(types, row)]
-
-                # Build dictionay if headers supplied or tuples if missing headers
-                if has_headers:
-                    record = dict(zip(header, row))
-                else:
-                    record = tuple(row)
-                records.append(record)
-            except ValueError as e:
-                if not silence_errors:
-                    print(f"Error processing row {rowno}: {row}")
-                    print(f"\tError Message: {e}")
+                record = tuple(row)
+            records.append(record)
+        except ValueError as e:
+            if not silence_errors:
+                print(f"Error processing row {rowno}: {row} > Reason: {e}")
     return records
